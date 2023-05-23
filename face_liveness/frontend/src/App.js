73d10c13599ad3da32e24a9e1dd5a4dc3d071903
Greywind299792458 <elliot.ganty@heig-vd.ch>
2023-05-23 23:03:58 +0200

    wip - frontend

--- face_liveness/frontend/src/App.js
+++ face_liveness/frontend/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
+import { Loader, ThemeProvider } from '@aws-amplify/ui-react';
 
 function App() {
+  const [loading, setLoading] = React.useState(true);
+  const [createLivenessApiData, setCreateLivenessApiData] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchCreateLiveness = async () => {
+     
+      // appel à notre API : on récupère le session id
+      const response  = await fetch('http://localhost:5000/getsessionid');
+      const data      = await response.json();
+      console.log(data);
+      setCreateLivenessApiData(data);
+      setLoading(false);
+    };
+
+    fetchCreateLiveness();
+  }, []);
+
+  const handleAnalysisComplete = async () => {
+    /*
+     * TODO
+     */
+    const response = await fetch(
+      `/api/get?sessionId=${createLivenessApiData.sessionId}`
+    );
+    const data = await response.json();
+
+    /*
+     * Note: The isLive flag is not returned from the GetFaceLivenessSession API
+     * This should be returned from your backend based on the score that you
+     * get in response. Based on the return value of your API you can determine what to render next.
+     * Any next steps from an authorization perspective should happen in your backend and you should not rely
+     * on this value for any auth related decisions.
+     */
+    if (data.isLive) {
+      console.log('User is live');
+    } else {
+      console.log('User is not live');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='detector'>
+    <ThemeProvider> {loading ? (<Loader className='loader' /> ) : (
+        <FaceLivenessDetector   className="detector"
+          sessionId={createLivenessApiData.sessionId}
+          region="us-east-1"
+          onAnalysisComplete={handleAnalysisComplete}
+        />
+      )}
+    </ThemeProvider>
     </div>
   );
 }
