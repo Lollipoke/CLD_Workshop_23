5574a0d2d340efcc879cbf7185144f5d42d7945d
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2023-06-06 21:30:52 +0200

    fix frontend

--- face_liveness/frontend/src/index.js
+++ face_liveness/frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Liveness from "./liveness";
+import {Amplify} from "aws-amplify";
+import awsexports from "./aws-exports";
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsexports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Liveness>
+    </Liveness>
 );
 
